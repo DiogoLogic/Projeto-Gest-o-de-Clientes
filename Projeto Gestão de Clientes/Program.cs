3a3e4932c2534c238fcb974e57ad6fd492381c6e
DiogoLogic <diogoinformatica1@gmail.com>
2022-07-12 01:40:26 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Gestão de Clientes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Gestão_de_Clientes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
