12e0b7653eaf796ea3fbed69e8359f0d1abe4c78
DiogoLogic <diogoinformatica1@gmail.com>
2022-07-12 01:50:39 -0300

    iniciando menu

--- Projeto Gestão de Clientes/Program.cs
+++ Projeto Gestão de Clientes/Program.cs
@@ -8,8 +8,14 @@ namespace Projeto_Gestão_de_Clientes
 {
     internal class Program
     {
+        enum Menu {Listagem = 1, adcionar = 2, remover = 3, Sair = 4}
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Sistema de clientes - Bem vindo!\n");
+            Console.WriteLine("1-Listagem\n2-adcionar\n3-remover\n4-Sair");
+            Console.ReadLine();
+
         }
     }
 }
